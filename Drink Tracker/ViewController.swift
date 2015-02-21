ff108797d32af3150ee38c7af091ce4ef3017519
Scott Minter <scott.minter@forrent.com>
2015-02-21 08:51:15 -0500

    Initial app setup with readme

---
+++ Drink Tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Drink Tracker
+//
+//  Created by Scott Minter on 2/21/15.
+//  Copyright (c) 2015 Scott Minter Designs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
