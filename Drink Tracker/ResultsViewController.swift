896179fb0228922b1f74e83afb8720ff73ad31b8
Scott Minter <scott.minter@forrent.com>
2015-02-21 10:37:33 -0500

    Grid layout is correct.

--- Drink Tracker/ResultsViewController.swift
+++ Drink Tracker/ResultsViewController.swift
@@ -1,32 +1,33 @@
 //
-//  ViewController.swift
+//  ResultsViewController.swift
 //  Drink Tracker
 //
 //  Created by Scott Minter on 2/21/15.
 //  Copyright (c) 2015 Scott Minter Designs. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
+class ResultsViewController: UIViewController {
     
     func whichView() {
-        println("Tracking View")
+        println("Results View")
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     override func viewDidAppear(animated: Bool) {
         whichView()
     }
+    
 }
-
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Drink-Track
+
+
+Any W Any H
+- 600 x 535 is the total size i'm working with
+- 590 x 525 is the adjusted area with 10 between each square
+- 295 x 263 is each square
+
