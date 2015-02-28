cb3662451cd2e895ca6dd75ba9219d1730541911
Scott Minter <scott.minter@forrent.com>
2015-02-28 08:00:17 -0500

    Added Info View

--- Drink Tracker/ViewController.swift
+++ Drink Tracker/ViewController.swift
@@ -10,14 +10,118 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var infoButton: UIButton!
+    
     //Upper Left Button
     @IBOutlet weak var ULButton: UIButton!
     @IBOutlet weak var ULView: UIView!
     
-    var BeerIcon = UIImage(named: "beer_230_230.png")
+    
+    /**
+     *  Drink Processing Functions
+     */
+    func processBeer() {
+println("yay beer")
+    }
+    
+    func processWine() {
+println("yay wine")
+    }
+    
+    func processShot() {
+println("yay shot")
+    }
+    
+    func processMixer() {
+println("yay mixer")
+    }
+
+    
+    /*
+     * Drink Buttons Action
+     */
+    @IBAction func drinkSelected(sender: AnyObject) {
+        var buttonId = sender.restorationIdentifier!!
+        
+        let dateDict = getFormattedDate()
+println(dateDict)
+        
+        if buttonId == "beer" {
+            processBeer()
+        }
+        else if buttonId == "wine" {
+            processWine()
+        }
+        else if buttonId == "shot" {
+            processShot()
+        }
+        else if buttonId == "mixer" {
+            processMixer()
+        }
+    }
+    
+    func getFormattedDate()->Dictionary<String, Any> {
+        var dateObj = Dictionary<String, Any>()
+        
+        var date = NSDate()
+        let calendar = NSCalendar.currentCalendar()
+        let components = calendar.components(.CalendarUnitYear | .CalendarUnitMonth | .CalendarUnitWeekday | .CalendarUnitDay | .CalendarUnitHour | .CalendarUnitMinute | .CalendarUnitSecond, fromDate: date)
+        let year = components.year
+        let month = components.month
+        let dayAsInt = components.day
+        let dayOfWeekAsInt = components.weekday
+        let hour = components.hour
+        let minutes = components.minute
+        let seconds = components.second
+        
+        var dayAsStr = String()
+        switch dayOfWeekAsInt {
+        case 1:
+            dayAsStr = "Sunday"
+            break
+        case 2:
+            dayAsStr = "Monday"
+            break
+        case 3:
+            dayAsStr = "Tuesday"
+            break
+        case 4:
+            dayAsStr = "Wednesday"
+            break
+        case 5:
+            dayAsStr = "Thursday"
+            break
+        case 6:
+            dayAsStr = "Friday"
+            break
+        case 7:
+            dayAsStr = "Saturday"
+            break
+        default:
+            dayAsStr = ""
+            break
+        }
+        
+        dateObj["year"] = year
+        dateObj["month"] = month
+        dateObj["dayAsInt"] = dayAsInt
+        dateObj["dayOfWeekAsInt"] = dayOfWeekAsInt
+        dateObj["dayOfWeekAsStr"] = dayAsStr
+        dateObj["hour"] = hour
+        dateObj["minute"] = minutes
+        dateObj["seconds"] = seconds
+        
+        return dateObj
+    }
     
     func whichView() {
         println("Tracking View")
+    }
+    
+    //Draws a border around the info button
+    func makeInfoButtonCircle() {
+        infoButton.layer.borderWidth = 0.8
+        infoButton.layer.borderColor = UIColor.grayColor().CGColor
     }
     
     //Disables Portrait
@@ -29,6 +133,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        //makeInfoButtonCircle()
     }
 
     override func didReceiveMemoryWarning() {
